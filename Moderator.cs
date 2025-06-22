af8e4d30f27e26a6026d5c21fc7ac2008954ca2b
Fosfor0000 <145984858+Fosfor0000@users.noreply.github.com>
2025-06-22 22:22:34 +0300

    Laba2C#

---
+++ Moderator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UserManagementSystem.Models
+{
+    public class Moderator : User
+    {
+        public void ModerateContent()
+        {
+            Console.WriteLine("Content modarated.");
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine("Role: Moderator");
+        }
+    }
+}
---
+++ RegularUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UserManagementSystem.Models
+{
+    public class RegularUser : User
+    {
+        public void PostComment()
+        {
+            Console.WriteLine("Comment published.");
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine("Role: User ");
+        }
+    }
+}
